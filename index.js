3f592cbeb73c5ca425e778e3a24a88ac79de356b
mohlingzheng <mohlingzheng@gmail.com>
2023-01-23 11:54:55 +0800

    add comment, delete unwanted code and file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var gl, canvas;
 var obj = "cube";
 var object;
-var theta, thetaLoc, colorLoc;
 
 var points = [];
 var normals = [];
@@ -9,9 +8,9 @@ var normals = [];
 const cube_obj = "cube"
 const cylinder_obj = "cylinder"
 const sphere_obj = "sphere"
-const teapot_obj = "teapot"
 
-const eye = vec3(1.0, 0.0, 1.5);                        //3.4a
+// Changing the location and orientation of camera via LookAt function
+const eye = vec3(1.0, 0.0, 1.5);
 const at = vec3(0.0, 0.0, 0.0);
 const up = vec3(0.0, 0.0, 1.0);
 
@@ -20,10 +19,14 @@ const fov = 55;
 const near = -0.3;
 const far = 5;
 
-var lightPosition = vec4(1.0, 1.0, 1.0, 1.0);    //3.2b, 3.2c, 3.2d
-var lightAmbient = vec4(0.5, 0.2, 0.2, 1.0);      //3.2a
+// Changing of position of the light
+var lightPosition = vec4(1.0, 1.0, 1.0, 1.0);   // Change the 4th parameter for point/distance light
+// Changing of color for selected light, 
+var lightAmbient = vec4(0.5, 0.2, 0.2, 1.0);
 var lightDiffuse = vec4(1.0, 0.5, 1.0, 1.0);
 var lightSpecular = vec4(1.0, 0.5, 1.0, 1.0);
+
+// Variables used to link to vertex shader
 var lightPositionLoc;
 var ambientProduct;
 var diffuseProduct;
@@ -38,16 +41,21 @@ var modelViewMatrix;
 var projectionMatrix;
 var modelViewMatrixLoc, projectionMatrixLoc;
 
-var materialAmbient = vec4(1.0, 1.0, 1.0, 1.0);   //3.3a
-var materialDiffuse = vec4(0.4, 0.3, 0.4, 1.0);   //3.3b coefficient is a float (0 to 1) multiply with diffuse, ambient, specular
-var materialSpecular = vec4(0.0, 0.4, 0.4, 1.0);  
-var materialShininess = 100.0;                    //3.3c
+// Change the material property in terms of ambient, diffuse, specular
+var materialAmbient = vec4(1.0, 1.0, 1.0, 1.0);
+var materialDiffuse = vec4(0.4, 0.3, 0.4, 1.0);
+var materialSpecular = vec4(0.0, 0.4, 0.4, 1.0); 
 
+// Change the shininess property of object's surface
+var materialShininess = 100.0;
+
+// Set the reflection coefficient for material ambient, diffuse, specular
 var Kd = 1.0;
 var Ka = 1.0;
 var Ks = 1.0;
 var KaLoc, KdLoc, KsLoc;
 
+// Start the program
 window.onload = function init()
 {
     canvas = document.getElementById("gl-canvas");
@@ -62,6 +70,7 @@ window.onload = function init()
     render();
 }
 
+// Load vertex and fragment shader
 function WebGLSetup(){
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
@@ -72,6 +81,7 @@ function WebGLSetup(){
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
+    // Determine the shape to be displayed and create the object in the scene
     if (obj == cube_obj){
       object = cube(1.0);
     }
@@ -82,26 +92,15 @@ function WebGLSetup(){
       object = sphere(5);
       object.scale(0.8, 0.8, 0.8);
     }
-    else if (obj == teapot_obj){
-      object = teapot(3);
-      object.scale(0.25, 0.25, 0.25);
-    }
-
     object.rotate(45, [1, 1, 1]);
 
-    theta = [0.0, 0.0, 0.0];
-
+    // Retrieve the points and normals of the objects from respective js by using the attribute provided
     points = []
     normals = []
-    if (obj != teapot_obj){
-      points = object.TriangleVertices;
-      normals = object.TriangleNormals;
-    }
-    else{
-      points = object.TriangleVertices;
-      normals = object.Normals;
-    }
+    points = object.TriangleVertices;
+    normals = object.TriangleNormals;
 
+    // Create buffer for points and normals
     var nBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(normals), gl.STATIC_DRAW );
@@ -114,43 +113,48 @@ function WebGLSetup(){
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
 
+    // Link attributes for points
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
+    // Link uniform model view
     modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
 
-    // var aspectRatio = gl.canvas.width / gl.canvas.height;
-    // projectionMatrix = perspective(fov, aspectRatio, near, far);
+    // Calculate and link projectionMatrix
     projectionMatrix = ortho(-volume, volume, -volume, volume, near, far);
     projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
 
-    thetaLoc = gl.getUniformLocation(program, "theta");
-
+    // Link attribute for light position
     lightPositionLoc = gl.getUniformLocation(program, "lightPosition");
 
+    // Multiple light ambient and material ambient to get the product
     ambientProduct = mult(lightAmbient, materialAmbient);
     ambientProductLoc = gl.getUniformLocation(program, "ambientProduct");
-
+    // Multiple light diffuse and material diffuse to get the product
     diffuseProduct = mult(lightDiffuse, materialDiffuse);
     diffuseProductLoc = gl.getUniformLocation(program, "diffuseProduct");
-
+    // Multiple light specular and material specular to get the product
     specularProduct = mult(lightSpecular, materialSpecular);
     specularProductLoc = gl.getUniformLocation(program, "specularProduct");
 
+    // Link to shininess of the material
     shininessLoc = gl.getUniformLocation(program, "shininess");
 
+    // Link to coefficient of reflection
     KaLoc = gl.getUniformLocation(program, "Ka");
     KdLoc = gl.getUniformLocation(program, "Kd");
     KsLoc = gl.getUniformLocation(program, "Ks");
 }
 
+// Button interaction for frontend
 function buttonInteraction(){
+  // Get the required shape and setup the scene again
   document.getElementById("object").oninput = function(){
     obj = this.value
     WebGLSetup()
   }
-
+  // Get the required light diffuse and update the value
   document.getElementById("light_diffuse").oninput = function(){
     var tempcolor = this.value;
     var rgb = convertHexToRGB(tempcolor);
@@ -159,6 +163,7 @@ function buttonInteraction(){
     lightDiffuse[2] = rgb.z
     diffuseProduct = mult(lightDiffuse, materialDiffuse);
   }
+  // Get the required light ambient and update the value
   document.getElementById("light_ambient").oninput = function(){
     var tempcolor = this.value;
     var rgb = convertHexToRGB(tempcolor);
@@ -167,6 +172,7 @@ function buttonInteraction(){
     lightAmbient[2] = rgb.z
     ambientProduct = mult(lightAmbient, materialAmbient);
   }
+  // Get the required light specular and update the value
   document.getElementById("light_specular").oninput = function(){
     var tempcolor = this.value;
     var rgb = convertHexToRGB(tempcolor);
@@ -176,6 +182,7 @@ function buttonInteraction(){
     specularProduct = mult(lightSpecular, materialSpecular);
   }
 
+  // Get the required light type by toggling the value from 0 to 1 or vice versa
   document.getElementById("light_type").onclick = function(){
     type = lightPosition[3]
     if(type == 1.0){
@@ -190,6 +197,7 @@ function buttonInteraction(){
     }
   }
 
+  // Turn on or off the light
   document.getElementById("on_off").onclick = function(){
     on = lightPosition[0]
     if(on >= 1.0){
@@ -212,6 +220,7 @@ function buttonInteraction(){
     }
   }
 
+  // Adjust position and orientation of camera
   document.getElementById("cam_angle_eye").oninput = function(){
     eye[0] = this.value;
   }
@@ -223,6 +232,7 @@ function buttonInteraction(){
     up[1] = this.value;
   }
   
+  // Adjust the position of light source
   document.getElementById("light_X").oninput = function(){
     lightPosition[0] = this.value;
   }
@@ -233,6 +243,7 @@ function buttonInteraction(){
     lightPosition[2] = this.value;
   }
 
+  // Change the diffuse of material 
   document.getElementById("material_diffuse").oninput = function(){
     var tempcolor = this.value;
     var rgb = convertHexToRGB(tempcolor);
@@ -241,6 +252,7 @@ function buttonInteraction(){
     materialDiffuse[2] = rgb.z
     diffuseProduct = mult(lightDiffuse, materialDiffuse);
   }
+  // Change the ambient of material 
   document.getElementById("material_ambient").oninput = function(){
     var tempcolor = this.value;
     var rgb = convertHexToRGB(tempcolor);
@@ -249,6 +261,7 @@ function buttonInteraction(){
     materialAmbient[2] = rgb.z
     ambientProduct = mult(lightAmbient, materialAmbient);
   }
+  // Change the specular of material 
   document.getElementById("material_specular").oninput = function(){
     var tempcolor = this.value;
     var rgb = convertHexToRGB(tempcolor);
@@ -258,16 +271,20 @@ function buttonInteraction(){
     specularProduct = mult(lightSpecular, materialSpecular);
   }
 
+  // Update the diffuse coefficient of reflection
   document.getElementById("coe_material_diffuse").oninput = function(){
     Kd = this.value;
   }
+  // Update the ambient coefficient of reflection
   document.getElementById("coe_material_ambient").oninput = function(){
     Ka = this.value;
   }
+  // Update the specular coefficient of reflection
   document.getElementById("coe_material_specular").oninput = function(){
     Ks = this.value;
   }
 
+  // Update the shininess of the object
   document.getElementById("material_shininess").oninput = function(){
     materialShininess = this.value;
   }
@@ -278,13 +295,14 @@ function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT );
 
+    // Pass the value to vertex shader
     gl.uniform4fv(lightPositionLoc, flatten(lightPosition));
     gl.uniform4fv(ambientProductLoc, flatten(ambientProduct));
     gl.uniform4fv(diffuseProductLoc, flatten(diffuseProduct));
     gl.uniform4fv(specularProductLoc, flatten(specularProduct));
     gl.uniform1f(shininessLoc, materialShininess);
 
-    // modelViewMatrix = mat4();
+    // Get the latest lookAt angle and pass to vertex shader
     modelViewMatrix = lookAt(eye, at, up);
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
     gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
@@ -293,28 +311,9 @@ function render()
     gl.uniform1f(KdLoc, Kd);
     gl.uniform1f(KsLoc, Ks);
 
-    // eye[0] += 0.01;
-    // console.log(eye[0])
-    // eye[1] += 0.01;
-    // if (eye[0] > 2 * Math.PI){
-    //   eye[0] -= 2 * Math.PI;
-    // }
-    // if (eye[1] > 2 * Math.PI){
-    //   eye[1] -= 2 * Math.PI;
-    // }
-    
-    // theta[0] += 0.5;
-    // theta[1] += 1.0;
-
-    // if (theta[1] > 360.0) {
-    //   theta[1] -= 360.0;
-    // }
-    // if (theta[0] > 360.0) {
-    //   theta[0] -= 360.0;
-    // }
-
-    gl.uniform3fv(thetaLoc, flatten(theta));
+    // Draw the shape accordingly
     gl.drawArrays( gl.TRIANGLES, 0, points.length );
 
+    // Re-render the scene
     requestAnimFrame( render );
 }
